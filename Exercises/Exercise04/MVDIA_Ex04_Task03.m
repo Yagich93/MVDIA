5ee27aad16d32d3c71b6c9a8eb4746ba80d22d19
Yagich <yagich@bk.ru>
2016-02-08 19:17:58 +0200

    Add img hist transformation to the desired.
    Add transformation of image hist.
    Add new image and hist plotting.

--- Exercises/Exercise04/MVDIA_Ex04_Task03.m
+++ Exercises/Exercise04/MVDIA_Ex04_Task03.m
@@ -24,10 +24,13 @@ for i = n/2:n
 end
 hgram = hgram/sum(hgram)*numel(img);
 
+% New image.
+imgn = histeq(img,hgram);
+
 % Plotting.
 figure('name', 'Histograms');
 subx = 2;
-suby = 3;
+suby = 4;
 subc = 0;
 % Original img.
 subc = subc + 1;
@@ -54,3 +57,11 @@ subc = subc + 1;
 subplot(suby, subx, subc);
 plot(1:n,hgram);
 axis([0 n 0 max(hgram)]);
+% New img.
+subc = subc + 1;
+subplot(suby, subx, subc);
+imshow(imgn);
+% New hist.
+subc = subc + 1;
+subplot(suby, subx, subc);
+imhist(imgn);
