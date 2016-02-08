0f8191fe0cd83289e8e8ba76fa04fc163f60a719
Yagich <yagich@bk.ru>
2016-02-08 18:45:54 +0200

    Add histogram equalization to Ex04 Task03.
    Add histogram equalization.
    Add plotting of the new image and the new hist.

--- Exercises/Exercise04/MVDIA_Ex04_Task03.m
+++ Exercises/Exercise04/MVDIA_Ex04_Task03.m
@@ -10,10 +10,13 @@ img = rgb2gray(img);
 % Histogram.
 [counts,binLocations] = imhist(img);
 
+% Histogram equalization.
+[imgeh, ~] = histeq(img);
+
 % Plotting.
 figure('name', 'Histograms');
 subx = 2;
-suby = 1;
+suby = 2;
 subc = 0;
 % Original img.
 subc = subc + 1;
@@ -23,3 +26,11 @@ imshow(img);
 subc = subc + 1;
 subplot(suby, subx, subc);
 imhist(img);
+% Img with equalized hist.
+subc = subc + 1;
+subplot(suby, subx, subc);
+imshow(imgeh);
+% Equalized hist.
+subc = subc + 1;
+subplot(suby, subx, subc);
+imhist(imgeh);
