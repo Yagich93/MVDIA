5f818440cc28503f3831a268473733675fa3897f
Yagich <yagich@bk.ru>
2016-02-08 18:16:04 +0200

    Add filtering with different parameters in Ex04 Task02.
    Add Gabor parameter arrays.
    Add eye rectangle on image.
    Add plotting magnitude, eyes or both.

--- Exercises/Exercise04/MVDIA_Ex04_Task02.m
+++ Exercises/Exercise04/MVDIA_Ex04_Task02.m
@@ -8,21 +8,31 @@ img = imread('MVDIA_Exercise04_images\Lena.png');
 img = rgb2gray(img);
 
 % Gabor filtering.
-wv = 5;
-or = 90;
-[mag,phase] = imgaborfilt(img,wv,or);
+wv = 4:2:16;
+or = 0:15:180;
+g = gabor(wv, or);
+[mag,phase] = imgaborfilt(img,g);
+
+% Eye rectangle.
+er = [240, 250, 360, 280];
 
 % Plotting images.
 figure('name', 'Gabor filtering');
-subx = 3;
-suby = 1;
-plotI = {img, mag, phase};
-plotTitles = {'Original', 'Gabor magnitude', 'Gabor phase'};
+subx = length(wv);
+suby = length(or);
 colormap gray;
-for subc = 1:size(plotI, 2);
+for subc = 1:size(mag, 3);
     subplot(suby, subx, subc);
-    image(plotI{subc}, 'CDataMapping', 'scaled');
-    title(plotTitles{subc});
-    axis equal;
-    axis([0, 512, 0, 512]);
+    
+    % Show magnitudes in eye rectangle.
+    %image(mag(er(2):er(4),er(1):er(3),subc), 'CDataMapping', 'scaled');
+    
+    % Show eyes in eye rectangle.
+    %image(img(er(2):er(4),er(1):er(3)), 'CDataMapping', 'scaled');
+    
+    % Show pair of magnitudes and eyes in eye rectangle.
+    imshowpair(mag(er(2):er(4),er(1):er(3),subc),...
+        img(er(2):er(4),er(1):er(3)), 'falsecolor');
+    
+    axis([1, er(3)-er(1), 1, er(4)-er(2)]);
 end
