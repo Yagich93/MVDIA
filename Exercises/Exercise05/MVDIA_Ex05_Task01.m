1a9672dc48f6fe7b3991b027a42124b9348421f3
Yagich <yagich@bk.ru>
2016-02-15 12:06:28 +0200

    Add Rayleigh distribution fitting to Ex05 Task01.

--- Exercises/Exercise05/MVDIA_Ex05_Task01.m
+++ Exercises/Exercise05/MVDIA_Ex05_Task01.m
@@ -12,4 +12,10 @@ imgroi = img(rc(2):rc(4),rc(1):rc(3));
 
 % Noise histogram.
 [counts,binLocations] = imhist(imgroi);
-imhist(imgroi);
+
+% Fit Rayleigh distribution.
+ft = fittype('c*(x>=a)*2/b*(x-a)*exp(-(x-a)^2/b)',...
+    'coefficients',{'a','b','c'});
+myfit = fit(binLocations, counts, ft, 'Lower',[0,0,0],...
+    'Upper',[255,100000,100000],'StartPoint',[128,5,1]);
+plot(myfit, binLocations, counts);
