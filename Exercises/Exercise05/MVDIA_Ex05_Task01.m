95a42cf0e1f13dc7651c093e381ba421e656224a
Yagich <yagich@bk.ru>
2016-02-15 11:05:00 +0200

    Add noise histogram calculation.

--- Exercises/Exercise05/MVDIA_Ex05_Task01.m
+++ Exercises/Exercise05/MVDIA_Ex05_Task01.m
@@ -9,4 +9,7 @@ img = imread('MVDIA_Exercise05_images/Lena_n.png');
 % Flat region of interest.
 rc = [110 10 290 20];
 imgroi = img(rc(2):rc(4),rc(1):rc(3));
-imshow(imgroi);
+
+% Noise histogram.
+[counts,binLocations] = imhist(imgroi);
+imhist(imgroi);
