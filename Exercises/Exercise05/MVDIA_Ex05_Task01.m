b9af4cf8cf424c552cc7f64b5660a8e5a5251b46
Yagich <yagich@bk.ru>
2016-02-15 11:02:09 +0200

    Add img loading and roi selection to Ex05 Task01.

---
+++ Exercises/Exercise05/MVDIA_Ex05_Task01.m
@@ -0,0 +1,12 @@
+% Cleaning.
+clc;
+clearvars;
+close all;
+
+% Load image.
+img = imread('MVDIA_Exercise05_images/Lena_n.png');
+
+% Flat region of interest.
+rc = [110 10 290 20];
+imgroi = img(rc(2):rc(4),rc(1):rc(3));
+imshow(imgroi);
