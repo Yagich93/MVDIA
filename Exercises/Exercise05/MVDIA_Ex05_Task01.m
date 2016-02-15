c26ff53f72bb7275373b6346205c7b6b470480fc
Yagich <yagich@bk.ru>
2016-02-15 12:09:17 +0200

    Add comparison plotting to Ex05 Task01.

--- Exercises/Exercise05/MVDIA_Ex05_Task01.m
+++ Exercises/Exercise05/MVDIA_Ex05_Task01.m
@@ -17,5 +17,10 @@ imgroi = img(rc(2):rc(4),rc(1):rc(3));
 ft = fittype('c*(x>=a)*2/b*(x-a)*exp(-(x-a)^2/b)',...
     'coefficients',{'a','b','c'});
 myfit = fit(binLocations, counts, ft, 'Lower',[0,0,0],...
-    'Upper',[255,100000,100000],'StartPoint',[128,5,1]);
+    'Upper',[255,100000,100000],'StartPoint',[128,100,100]);
+
+% Plotting.
+subplot(1, 2, 1);
+imhist(imgroi);
+subplot(1, 2, 2);
 plot(myfit, binLocations, counts);
