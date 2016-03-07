c5b90c931b3ad7c8c616a1100bb9a158c6fa34c4
Yagich <yagich@bk.ru>
2016-03-07 21:44:15 +0300

    Ex06_Task02: Add task commentary.

--- Exercises/Exercise06/MVDIA_Ex06_Task03.m
+++ Exercises/Exercise06/MVDIA_Ex06_Task03.m
@@ -1,4 +1,24 @@
 % Motion blur reduction.
+%
+% NOTE: The script might not run because of the forward/backward-slash
+% issue in paths to images. Choose the correct slashes, according to your
+% system.
+% 
+% The input image for the experiment seems to have experienced motion blur.
+% This blur is manually estimated and then used to reconstruct original
+% image in Wiener filtering.
+%
+% Reconstruction with the assumption of the degradation without noise gives
+% highly noised result. Although, the contours of the object on the image
+% seems to become correct, without motion blur.
+%
+% Estimation of the noise as the variance of the flat region and
+% utilization of it to estimate noise to signal ratio makes the result
+% clear, lacking intensive noise. However, some artifacts can still be
+% seen: dim duplicates of the objects. This might be due to the
+% imperfections of the chosen degradation function and noise estimations,
+% as well as the impossibility of the ideal restoration by the given
+% algorithm.
 
 % Cleaning.
 clc;
