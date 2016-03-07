4be6a41c8c45778ee1c4be2bdf94d683377f3026
Yagich <yagich@bk.ru>
2016-03-07 17:48:23 +0300

    Ex06_Task01: Add median filtering.

--- Exercises/Exercise06/MVDIA_Ex06_Task01.m
+++ Exercises/Exercise06/MVDIA_Ex06_Task01.m
@@ -6,11 +6,14 @@ close all;
 % Load image.
 img = imread('MVDIA_Exercise06_images/ex6t1.tif');
 
+% Median filter.
+imgm = medfilt2(img, 'symmetric');
+
 % Plotting.
 figure('name', 'Median filtering');
-plotI = {img};
-plotTitles = {'Original'};
-subx = 1;
+plotI = {img, imgm};
+plotTitles = {'Original', 'Median 3x3 (x1)'};
+subx = size(plotI, 2);
 suby = 1;
 for subc = 1:size(plotI, 2);
     subplot(suby, subx, subc);
