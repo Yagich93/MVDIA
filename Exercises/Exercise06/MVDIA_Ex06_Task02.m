2e2d4baa579ff542c82bb93cfe2ce0b6fd901c1b
Yagich <yagich@bk.ru>
2016-03-07 19:38:40 +0300

    Ex06_Task02: Remove secondary peaks.

--- Exercises/Exercise06/MVDIA_Ex06_Task02.m
+++ Exercises/Exercise06/MVDIA_Ex06_Task02.m
@@ -11,10 +11,19 @@ img = imread('MVDIA_Exercise06_images/Lena_fn.png');
 % Fourier transform.
 F = fftshift(fft2(img));
 
+% Remove secondary peaks.
+Fnp = F;
+Fnp(225, 129) = 0;
+Fnp(289, 129) = 0;
+Fnp(225, 385) = 0;
+Fnp(289, 385) = 0;
+imgnp = uint8(ifft2(ifftshift(Fnp)));
+
 % Plotting.
 figure('name', 'Periodic noise reduction');
-plotI = {img, log(abs(F))./max(max(log(abs(F))))};
-plotTitles = {'Original', 'Original spectrum'};
+plotI = {img, log(abs(F))./max(max(log(abs(F)))),...
+         imgnp, log(abs(Fnp))./max(max(log(abs(Fnp))))};
+plotTitles = {'Original', 'Original spectrum', 'No peaks', 'No peaks spectrum'};
 subx = ceil(size(plotI, 2)/1);
 suby = 1;
 for subc = 1:size(plotI, 2);
