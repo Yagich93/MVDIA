8da8a8baeaf88b5cdb9ba866039d3e138f903183
Yagich <yagich@bk.ru>
2016-03-07 17:51:52 +0300

    Ex06_Task01: Add x2 and x3 median filtering.

--- Exercises/Exercise06/MVDIA_Ex06_Task01.m
+++ Exercises/Exercise06/MVDIA_Ex06_Task01.m
@@ -8,11 +8,13 @@ img = imread('MVDIA_Exercise06_images/ex6t1.tif');
 
 % Median filter.
 imgm = medfilt2(img, 'symmetric');
+imgm2 = medfilt2(imgm, 'symmetric');
+imgm3 = medfilt2(imgm2, 'symmetric');
 
 % Plotting.
 figure('name', 'Median filtering');
-plotI = {img, imgm};
-plotTitles = {'Original', 'Median 3x3 (x1)'};
+plotI = {img, imgm, imgm2, imgm3};
+plotTitles = {'Original', 'Median 3x3 (x1)', 'Median 3x3 (x2)', 'Median 3x3 (x3)'};
 subx = size(plotI, 2);
 suby = 1;
 for subc = 1:size(plotI, 2);
