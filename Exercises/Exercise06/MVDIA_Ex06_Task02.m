739532c86e94ebddc54b637f93774e6623fb5fc3
Yagich <yagich@bk.ru>
2016-03-07 20:15:20 +0300

    Ex06_Task02: Add task commentary.

--- Exercises/Exercise06/MVDIA_Ex06_Task02.m
+++ Exercises/Exercise06/MVDIA_Ex06_Task02.m
@@ -1,4 +1,37 @@
 % Periodic noise reduction.
+%
+% NOTE: The script might not run because of the forward/backward-slash
+% issue in paths to images. Choose the correct slashes, according to your
+% system.
+% 
+% The input image for the experiment has an intensive periodic noise.
+% The general shape of the image can be recognized, but the details cannot
+% be seen clearly. This periodic noise results in regular peaks in the
+% spectrum of the image. Those peaks also have additional vertical and
+% horizontal lines, emerging from them.
+%
+% Removal of the secondary spectral peaks only (the points with maximal
+% amplitude), but retaining the lines gives the following result. The
+% periodic noise lessens in the center of and image, but remains intensive
+% on the sides. The effect is seen more in the horizontal direction. The
+% shape of the noise stays the same.
+%
+% Removal of the whole vertical and horizontal lines in spectrum, having
+% the same coordinates as peaks, reduces the noise much more. There can be
+% seen almost no noise in the center of the image, but a light noise with
+% the same shape can still be seen on the sides.
+%
+% Removal of the 3-pixel-wide bands instead of the lines in spectrum lowers
+% the noise level even further: the remainings of the original noise can
+% only be seen in the corners of the image. At the same time, some
+% distortions are introduced due to the loss of information: not all the
+% values omitted from the spectrum contain noise - some of them hold actual
+% information.
+%
+% In order to get rid of the original noise completely and still retain the
+% informative part of the image (i.e. not introduce distortions), some more
+% complex shapes of the spectral filters should be proposed, removing only
+% corrupted spectral values.
 
 % Cleaning.
 clc;
