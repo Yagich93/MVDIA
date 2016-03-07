cfe0ca798065696573750c7eb83875415564c804
Yagich <yagich@bk.ru>
2016-03-07 21:33:47 +0300

    Ex06_Task03: Add Wiener filter, noise estimation.

--- Exercises/Exercise06/MVDIA_Ex06_Task03.m
+++ Exercises/Exercise06/MVDIA_Ex06_Task03.m
@@ -14,11 +14,18 @@ h = fspecial('motion', 200, 135);
 % Remove motion blur assuming no noise.
 imgwnn = deconvwnr(img, h, 0);
 
+% Remove motion blur with noise estimation.
+roic = [1090 1336 86 306];
+roi = img(roic(3):roic(4), roic(1):roic(2));
+nsre = var(double(roi(:))) / var(double(img(:)));
+imgwne = deconvwnr(img, h, nsre);
+
 % Plotting.
 figure('name', 'Motion blur reduction');
-plotI = {img, h/max(max(h)), imgwnn};
+plotI = {img, h/max(max(h)), imgwnn, imgwne};
 plotTitles = {'Original', 'Motion blur filter',...
-    'Wiener deconvolution, no noise'};
+    'Wiener deconvolution, no noise',...
+    'Wiener deconvolution, noise estimation'};
 subx = size(plotI, 2);
 suby = 1;
 for subc = 1:size(plotI, 2)
