addc2c3a0989989ddb2e80a195faf54656622077
Yagich <yagich@bk.ru>
2016-03-07 19:25:54 +0300

    Ex06_Task02: Plot original spectrum.

--- Exercises/Exercise06/MVDIA_Ex06_Task02.m
+++ Exercises/Exercise06/MVDIA_Ex06_Task02.m
@@ -8,12 +8,15 @@ close all;
 % Load image.
 img = imread('MVDIA_Exercise06_images/Lena_fn.png');
 
+% Fourier transform.
+F = fftshift(fft2(img));
+
 % Plotting.
 figure('name', 'Periodic noise reduction');
-plotI = {img};
-plotTitles = {'Original'};
+plotI = {img, log(abs(F))./max(max(log(abs(F))))};
+plotTitles = {'Original', 'Original spectrum'};
 subx = ceil(size(plotI, 2)/1);
-suby = ceil(size(plotI, 2)/1);
+suby = 1;
 for subc = 1:size(plotI, 2);
     subplot(suby, subx, subc);
     imshow(plotI{subc});
