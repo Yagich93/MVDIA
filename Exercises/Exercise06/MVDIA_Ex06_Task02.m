ab34bb2370999979068b30caaeee0f854f94ced1
Yagich <yagich@bk.ru>
2016-03-07 19:48:20 +0300

    Ex06_Task02: Remove secondary spectral lines.

--- Exercises/Exercise06/MVDIA_Ex06_Task02.m
+++ Exercises/Exercise06/MVDIA_Ex06_Task02.m
@@ -19,13 +19,24 @@ Fnp(225, 385) = 0;
 Fnp(289, 385) = 0;
 imgnp = uint8(ifft2(ifftshift(Fnp)));
 
+% Remove secondary lines.
+Fnl = F;
+Fnl(225, :) = 0;
+Fnl(289, :) = 0;
+Fnl(:, 129) = 0;
+Fnl(:, 385) = 0;
+imgnl = uint8(ifft2(ifftshift(Fnl)));
+
+% Function for spectrum processing for plotting.
+sp = @(F)log(abs(F))./max(max(log(abs(F))));
+
 % Plotting.
 figure('name', 'Periodic noise reduction');
-plotI = {img, log(abs(F))./max(max(log(abs(F)))),...
-         imgnp, log(abs(Fnp))./max(max(log(abs(Fnp))))};
-plotTitles = {'Original', 'Original spectrum', 'No peaks', 'No peaks spectrum'};
-subx = ceil(size(plotI, 2)/1);
-suby = 1;
+plotI = {img, imgnp, imgnl, sp(F), sp(Fnp), sp(Fnl)};
+plotTitles = {'Original', 'Original spectrum', 'No peaks',...
+              'No peaks spectrum', 'No lines', 'No lines spectrum'};
+subx = ceil(size(plotI, 2)/2);
+suby = 2;
 for subc = 1:size(plotI, 2);
     subplot(suby, subx, subc);
     imshow(plotI{subc});
