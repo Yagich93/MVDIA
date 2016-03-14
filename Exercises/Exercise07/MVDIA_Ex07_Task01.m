665b956d3e1a7f97b1bd86d8073db7d4ff306fb1
Yagich <yagich@bk.ru>
2016-03-14 16:21:10 +0200

    Ex07_Task01: Generate image.
    Correct profile arrays indexing.
    Generate image.
    Plot image.

--- Exercises/Exercise07/MVDIA_Ex07_Task01.m
+++ Exercises/Exercise07/MVDIA_Ex07_Task01.m
@@ -7,14 +7,25 @@ close all;
 N = 512;
 
 % Create profiles.
-prof(1,:) = fliplr(0:1/(N-1):1);
-prof(2,1:N/2) = 0:2/(N-1):1;
-prof(2,N/2+1:N) = fliplr(0:2/(N-1):1);
-prof(3,:) = 0:1/(N-1):1;
+prof(1,:,1) = fliplr(0:1/(N-1):1);
+prof(1,1:N/2,2) = 0:2/(N-1):1;
+prof(1,N/2+1:N,2) = fliplr(0:2/(N-1):1);
+prof(1,:,3) = 0:1/(N-1):1;
 
+% Generate image.
+img = repmat(prof, N, 1, 1);
+
+% Plotting.
 % Plot profiles.
+figure('name', 'Image with profiles');
+subplot(2,1,1);
 hold on;
-plot(prof(1,:));
-plot(prof(2,:));
-plot(prof(3,:));
+plot(prof(1,:,1));
+plot(prof(1,:,2));
+plot(prof(1,:,3));
 hold off;
+title('Profiles');
+% Plot image.
+subplot(2,1,2);
+imshow(img);
+title('Image');
