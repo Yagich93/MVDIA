8fae4f13d0765d9cea5a3bb516b21cee215a16cb
Yagich <yagich@bk.ru>
2016-03-14 17:05:40 +0200

    Ex07_Task02: Convert to HSL.
    Load components to one matrix.
    Convert to HSL.
    Plot image.

--- Exercises/Exercise07/MVDIA_Ex07_Task02.m
+++ Exercises/Exercise07/MVDIA_Ex07_Task02.m
@@ -4,14 +4,17 @@ clearvars;
 close all;
 
 % Load images.
-imgh = imread('MVDIA_Exercise07_images/ex7t2H.tif');
-imgs = imread('MVDIA_Exercise07_images/ex7t2S.tif');
-imgl = imread('MVDIA_Exercise07_images/ex7t2I.tif');
+img_hsl(:,:,1) = imread('MVDIA_Exercise07_images/ex7t2H.tif');
+img_hsl(:,:,2) = imread('MVDIA_Exercise07_images/ex7t2S.tif');
+img_hsl(:,:,3) = imread('MVDIA_Exercise07_images/ex7t2I.tif');
+
+% Generate image.
+img = hsl2rgb(double(img_hsl)./255);
 
 % Plotting.
 figure('name', 'HSI colors');
-plotI = {imgh, imgs, imgl};
-plotTitles = {'Hue', 'Saturation', 'Lightness (Intensity)'};
+plotI = {img_hsl(:,:,1), img_hsl(:,:,2), img_hsl(:,:,3), img};
+plotTitles = {'Hue', 'Saturation', 'Lightness (Intensity)', 'Image'};
 subx = 3;
 suby = ceil(size(plotI, 2)/3);
 for subc = 1:size(plotI, 2)
