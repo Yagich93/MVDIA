f6d834a39cca9639b1e74a8bf721d11fd2c094a2
Yagich <yagich@bk.ru>
2016-04-11 16:08:27 +0300

    Ex11_Task02: Calculate GLCMs.

--- Exercises/Exercise11/MVDIA_Ex11_Task02.m
+++ Exercises/Exercise11/MVDIA_Ex11_Task02.m
@@ -8,6 +8,12 @@ i1 = imread('MVDIA_Exercise11_images/ex11t2a.tif');
 i2 = imread('MVDIA_Exercise11_images/ex11t2b.tif');
 i3 = imread('MVDIA_Exercise11_images/ex11t2c.tif');
 
+% Gray-level co-occurence matrices
+% Rule: one-pixel immediately to the right (default)
+glcm1 = graycomatrix(i1);
+glcm2 = graycomatrix(i2);
+glcm3 = graycomatrix(i3);
+
 % Plotting
 figure('name', 'MVDIA_Ex11_Task02');
 is = {i1, i2, i3};
